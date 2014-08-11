bc50e4f1c9758310dae76d0abc20a0e28fff4bb7
Magnus Hagmar <hagmarm@student.chalmers.se>
2014-08-11 12:32:04 +0200

    Now responds to received pings

--- src/msghandler.erl
+++ src/msghandler.erl
@@ -5,9 +5,20 @@
 loop(Sock) ->
 	receive
 		{message, Data} ->
-			io:format("NEW MESSAGE"),
 			io:format("~ts", [Data]),
+			spawn(fun() -> process_msg(Data, Sock) end),
 			loop(Sock);
 		_ ->
-			gjf
+			loop(Sock)
 	end.
+
+process_msg(<<"PING :", Data/binary>>, Sock) ->
+	send("PONG :" ++ Data, Sock);
+
+process_msg(_, _) ->
+	io:format("TJENNAAA"),
+	ok.
+
+send(Data, Sock) ->
+	io:format("Sending ~p~n", [Data]),
+	gen_tcp:send(Sock, Data).
