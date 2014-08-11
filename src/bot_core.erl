773d112f2c855494b98a0e1bc7622fc34b6269d9
Magnus Hagmar <hagmarm@student.chalmers.se>
2014-08-11 16:51:31 +0200

    The bot can now be started with the server or server and port specified

--- src/bot_core.erl
+++ src/bot_core.erl
@@ -1,16 +1,25 @@
 -module(bot_core).
 
--export([start/0]).
+-export([start/0, start/1, start/2]).
 
 start() ->
-	Server = "irc.underworld.no",
-	Port = 6667,
-	{ok, Sock} = gen_tcp:connect(Server, Port, [binary, {packet, 0}, {active, false}]),
-	gen_tcp:send(Sock, "NICK Frappebot\r\n"),
-	gen_tcp:send(Sock, "USER Frappebot 0 * :Testing bot\r\n"),
-	gen_tcp:send(Sock, "JOIN #TJENNA\r\n"),
-	MsgHandler = spawn(fun() -> msghandler:loop(Sock) end),
-	receive_loop(Sock, MsgHandler).
+	start("irc.underworld.no", 6667).
+
+start(Server) ->
+	start(Server, 6667).
+
+start(Server, Port) ->
+	case gen_tcp:connect(Server, Port, [binary, {packet, 0}, {active, false}]) of
+		{ok, Sock} ->
+			gen_tcp:send(Sock, "NICK Frappebot\r\n"),
+			gen_tcp:send(Sock, "USER Frappebot 0 * :Testing bot\r\n"),
+			gen_tcp:send(Sock, "JOIN #TJENNA\r\n"),
+			MsgHandler = spawn(fun() -> msghandler:loop(Sock) end),
+			receive_loop(Sock, MsgHandler);
+		_ ->
+			error
+	end.
+	
 
 receive_loop(Socket, MsgHandler) ->
 	case gen_tcp:recv(Socket, 0) of
