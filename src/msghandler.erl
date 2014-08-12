377a08cbe0290c93800475295ad3f2ebc3e0b4c2
Magnus Hagmar <hagmarm@student.chalmers.se>
2014-08-12 09:30:44 +0200

    Bot now automatically (tries to) give any new users voice

--- src/msghandler.erl
+++ src/msghandler.erl
@@ -5,7 +5,7 @@
 loop(Sock) ->
 	receive
 		{message, Data} ->
-			io:format("~ts", [Data]),
+			io:format("~p~n", [binary_to_list(Data)]),
 			spawn(fun() -> process_msg(Data, Sock) end),
 			loop(Sock);
 		_ ->
@@ -17,8 +17,9 @@ process_msg(<<"PING :", Data/binary>>, Sock) ->
 
 process_msg(BinaryData, Sock) ->
 	case binary:split(BinaryData, <<" ">>) of
-		[Prefix | Message] ->
-			io:format("Prefix: ~p~nMessage: ~p~n", [Prefix, Message]);
+		[Prefix | [Message | [] ]] ->
+			io:format("Prefix: ~p~nMessage: ~p~n", [Prefix, Message]),
+			handle_message(Prefix, Message, Sock);
 		_ ->
 			io:format("no~n")
 	end;
@@ -26,6 +27,18 @@ process_msg(BinaryData, Sock) ->
 process_msg(_, _) ->
 	ok.
 
+handle_message(<<":", Prefix/binary>>, <<"JOIN :", Channel/binary>>, Sock) ->
+	[User | [Host | [] ]] = binary:split(Prefix, <<"!">>),
+	ChannelName = stripCtrl(binary_to_list(Channel)),
+	Msg = "MODE " ++ ChannelName ++ " +v " ++ binary_to_list(User) ++ "\r\n",
+	send(Msg, Sock);
+
+handle_message(_, Aee, _) ->
+	io:format("~p~n", [Aee]).
+
 send(Data, Sock) ->
 	io:format("Sending ~p~n", [Data]),
 	gen_tcp:send(Sock, Data).
+
+stripCtrl(Data) ->
+	string:strip(string:strip(Data, right, $\n), right, $\r).
